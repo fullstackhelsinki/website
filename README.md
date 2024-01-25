a43908c948578ee1cee35eff2a015e3327685813
Julius Rajala <juliusrajala@gmail.com>
2024-01-25 19:57:43 +0200

    Vercel deployment config

---
+++ README.md
@@ -0,0 +1,9 @@
+# Full Stack Helsinki
+
+Helsinki Full-stack meetup's website.
+
+## Deployment
+
+- Deployed via Vercel using Astro's Vercel integration
+  - Uses `imageService` to generate correct image tags
+  - [Vercel Astro Integration](https://docs.astro.build/en/guides/integrations-guide/vercel/)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://fullstackhelsinki.fi",
   integrations: [tailwind(), icon()],
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
